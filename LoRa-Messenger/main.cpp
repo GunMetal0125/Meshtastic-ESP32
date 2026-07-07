1690461725a79eb7c3ee322d0f9a648f9d01b689
GunMetal0125 <andy.arroyo@aol.com>
2026-07-07 02:39:06 -0400

    Update main.cpp

--- LoRa-Messenger/main.cpp
+++ LoRa-Messenger/main.cpp
@@ -200,3 +200,107 @@ void loop() {
     display.display();
   }
 }
+#include <Arduino.h>
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <TinyGPSPlus.h>
+
+// OLED settings
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// GPS
+TinyGPSPlus gps;
+HardwareSerial GPSserial(1);  // UART1
+
+// SX1262 Pins
+#define LORA_SCK   18
+#define LORA_MISO  19
+#define LORA_MOSI  23
+#define LORA_CS     5
+#define LORA_RST   14
+#define LORA_DIO1  33
+#define LORA_BUSY  32
+
+#define LORA_FREQ  915E6
+
+void showText(const String &text) {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println(text);
+  display.display();
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  // OLED INIT
+  Wire.begin(21, 22);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  showText("LoRa Messenger\nGPS Init...");
+
+  // GPS INIT
+  GPSserial.begin(9600, SERIAL_8N1, 16, 17);
+  showText("GPS Ready");
+
+  // LoRa INIT
+  SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
+  LoRa.setPins(LORA_CS, LORA_RST, LORA_DIO1);
+
+  if (!LoRa.begin(LORA_FREQ)) {
+    showText("LoRa FAILED!");
+    while (true);
+  }
+
+  showText("LoRa Ready\nListening...");
+}
+
+void loop() {
+  // Read GPS data
+  while (GPSserial.available()) {
+    gps.encode(GPSserial.read());
+  }
+
+  // Send GPS every 5 seconds
+  static unsigned long lastSend = 0;
+  if (millis() - lastSend > 5000) {
+    lastSend = millis();
+
+    if (gps.location.isValid()) {
+      double lat = gps.location.lat();
+      double lon = gps.location.lng();
+
+      String msg = "GPS:" + String(lat, 6) + "," + String(lon, 6);
+
+      showText("Sending GPS...\n" + msg);
+
+      LoRa.beginPacket();
+      LoRa.print(msg);
+      LoRa.endPacket();
+
+      showText("Sent!\nListening...");
+    } else {
+      showText("GPS Invalid\nWaiting...");
+    }
+  }
+
+  // Receive LoRa packets
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    String incoming = "";
+    while (LoRa.available()) {
+      incoming += (char)LoRa.read();
+    }
+
+    Serial.println("Received: " + incoming);
+    showText("Received:\n" + incoming);
+  }
+}
